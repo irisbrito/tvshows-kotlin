a4d9b4164a2a4d5ffe88f3faf1fa4b5bea37d479
Iris <iris.souza@zup.com.br>
2021-05-21 15:07:01 -0300

    Adicionar classe manipulador de exceções

---
+++ src/main/kotlin/com/br/iris/exception/ExceptionHandler.kt
@@ -0,0 +1,23 @@
+package com.br.iris.exception
+
+import io.micronaut.context.annotation.Requires
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.annotation.Produces
+import io.micronaut.http.server.exceptions.ExceptionHandler
+import javax.inject.Singleton
+
+@Produces
+@Singleton
+@Requires(classes = [SerieNotFoundException::class, ExceptionHandler::class])
+class ExceptionHandler : ExceptionHandler<SerieNotFoundException?, HttpResponse<*>>  {
+
+    override fun handle(request: HttpRequest<*>?, exception: SerieNotFoundException?): HttpResponse<*> {
+        val errorMessage = ErrorMessage(
+            HttpStatus.BAD_REQUEST.toString(),400
+            ,"invalid arguments")
+
+        return HttpResponse.badRequest(errorMessage)
+    }
+}
