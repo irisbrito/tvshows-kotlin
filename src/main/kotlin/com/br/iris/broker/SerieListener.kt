771eb1d43685872f91d3d60a79703a7c054203b1
Iris <iris.souza@zup.com.br>
2021-06-17 18:16:38 -0300

    Remover @Body na SerieListener

--- src/main/kotlin/com/br/iris/broker/SerieListener.kt
+++ src/main/kotlin/com/br/iris/broker/SerieListener.kt
@@ -2,16 +2,19 @@ package com.br.iris.broker
 
 import com.br.iris.entity.Serie
 import com.br.iris.service.SerieServiceImpl
-import io.micronaut.http.annotation.Body
 import io.micronaut.nats.annotation.NatsListener
 import io.micronaut.nats.annotation.Subject
+import org.slf4j.LoggerFactory
 
 @NatsListener
 class SerieListener(private val serieService: SerieServiceImpl) {
 
+    private val logger = LoggerFactory.getLogger(SerieListener::class.java)
+
     @Subject("series")
-    fun receive(@Body serie: Serie) {
+    fun receive(serie: Serie) {
         serieService.create(serie)
+        logger.info("msg recebida")
         println("Got serie - $serie")
     }
 }
